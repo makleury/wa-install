50373a2a54671c504be5a49ba7bf8105c92c94b8
Jônatas <91859922+makleury@users.noreply.github.com>
2023-12-25 16:40:18 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# wa-install
-Whaticket Baileys | ISI
+<h1 align="center">Whaticket Baileys |ISI - Instituto de Serviços Intelectuais</h1>
+
+<div align="center">
+
+
+## Vamos instalar?
+
+FAZENDO DOWNLOAD DO INSTALADOR & INICIANDO A PRIMEIRA INSTALAÇÃO (USAR SOMENTE PARA PRIMEIRA INSTALAÇÃO):
+
+```bash
+sudo apt install -y git && git clone https://github.com/makleury/wa-install install && sudo chmod -R 777 ./install && cd ./install && sudo ./install_primaria
+```
+
+ACESSANDO DIRETORIO DO INSTALADOR & INICIANDO INSTALAÇÕES ADICIONAIS (USAR ESTE COMANDO PARA SEGUNDA OU MAIS INSTALAÇÃO:
+```bash
+cd && cd ./install && sudo ./install_instancia
+```
+
+
+## Para Instalação você precisa:
+
+Uma VPS Ubuntu 20.04 (Configuração recomendada: 3 VCPU's + 4 GB RAM)
+
+Subdominio para Frontend
+
+Subdominio para API - backoffice
+
+Email válido para certificação SSL
+
---
+++ utils/_banner.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Print banner art.
+
+#######################################
+# Print a board. 
+# Globals:
+#   BG_BROWN
+#   NC
+#   WHITE
+#   CYAN_LIGHT
+#   RED
+#   GREEN
+#   YELLOW
+# Arguments:
+#   None
+#######################################
+print_banner() {
+
+  clear
+
+  printf "\n\n"
+
+printf "${CYAN_LIGHT}";
+
+
+printf ${CYAN_LIGHT}" _____   ____   _____   \n";  
+printf ${CYAN_LIGHT}"|_   _| /  _ \ |_   _|  \n"; 
+printf ${CYAN_LIGHT}"  | |   \ \ \/   | |    \n";
+printf ${CYAN_LIGHT}"  | |    \ \     | |    \n";
+printf ${CYAN_LIGHT}"  | |     \ \    | |    \n";
+printf ${CYAN_LIGHT}" _| |_  /\_\ \  _| |_   \n";
+printf ${CYAN_LIGHT}"|_____| \____/ |_____|  \n";
+
+                                                                                                                                                         
+  printf "            \033[1;33m        © ISI - Instituto de Serviços Intelectuais\n";
+  printf "${NC}";
+
+  printf "\n"
+}
